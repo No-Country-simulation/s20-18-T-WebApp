eb119f39d87f61bcfca42bacce10d6fae6eea3d3
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2025-01-09 17:13:54 -0300

    First commit

---
+++ backend/newApp/src/test/java/com/s20_18_T_WebApp/newApp/NewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s20_18_T_WebApp.newApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
