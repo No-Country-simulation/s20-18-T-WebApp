2ef6a107b21233e13fa86304153a58be95abbaf6
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2025-01-13 18:29:45 -0300

    create user and habit entity.

---
+++ backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/habits/internal/domain/entity/Habit.java
@@ -0,0 +1,11 @@
+package com.s20_18_T_WebApp.backend.habits.internal.domain.entity;
+
+public class Habit {
+    /*
+      nombre, fecha, dias que se va a repetir(duracion del habito, puede ser para siempre o por un valor predefinido de dias. Si se termina de utilizar o no se usa por determinado tiempo se archiva.)
+    , dias de la semana. tipo de habito.
+    porcentaje de seguimiento,
+    dias de racha.
+    archivado: true o false.
+     */
+}
---
+++ backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/users/internal/domain/entity/User.java
@@ -0,0 +1,19 @@
+package com.s20_18_T_WebApp.backend.users.internal.domain.entity;
+
+import com.s20_18_T_WebApp.backend.shared.domain.entity.BaseEntity;
+import jakarta.persistence.Column;
+
+public class User extends BaseEntity {
+    //foto, nombre, apellido, fecha de nacimiento, email, password,los niveles del usuario(VO), TODO cuales son los niveles.
+    //puntos del usuario para llegar al siguiente nivel.(int?)
+    //logros del usuario TODO que logros tiene el usuario
+
+    @Column(name = "first-name", nullable = false, unique = true)
+    private String firstname;
+
+    @Column(name = "last-name", nullable = false, unique = true)
+    private String lastname;
+
+    @Column(name = "photo, nullable = true")
+    private String photo;
+}
