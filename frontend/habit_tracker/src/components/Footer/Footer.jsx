975e3daca6527a1f73a4c1d598df69d87a8ea125
cesar <cesarhernanruscica@gmail.com>
2025-01-12 21:23:10 -0300

    react basic template

--- frontend/habit_tracker/src/components/Footer/Footer.jsx
+++ frontend/habit_tracker/src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Footer.module.css';
 
 export const Footer = () => {
   return (
-    <div>
+    <footer>
       <h2>Footer</h2>
       <p>Hecho por el equipo S20-18-T-WebApp de No Country 2025</p>
-    </div>
+    </footer>
   )
 }
