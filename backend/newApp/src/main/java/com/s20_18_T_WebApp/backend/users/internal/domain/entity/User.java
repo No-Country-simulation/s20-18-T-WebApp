242631277de707282a1ea7dd58e6c0ee56dfc332
KevinLautaroSoto <kevinlautarosoto@gmail.com>
2025-01-14 16:53:44 -0300

    Add properties for the user entity and tasks to complete in the future updates.

--- backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/users/internal/domain/entity/User.java
+++ backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/users/internal/domain/entity/User.java
@@ -3,11 +3,26 @@ package com.s20_18_T_WebApp.backend.users.internal.domain.entity;
 import com.s20_18_T_WebApp.backend.shared.domain.entity.BaseEntity;
 import com.s20_18_T_WebApp.backend.shared.domain.vo.Image;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
+import java.util.List;
+
+@Entity
+@SuperBuilder
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Table(name = "users")
 public class User extends BaseEntity {
-    //foto, nombre, apellido, fecha de nacimiento, email, password,los niveles del usuario(VO), TODO cuales son los niveles.
+    //fecha de nacimiento, email, password,los niveles del usuario(VO), TODO cuales son los niveles.
     //puntos del usuario para llegar al siguiente nivel.(int?)
     //logros del usuario TODO que logros tiene el usuario
+    //password va a ir en la entidad dentro de la carpeta auth.
 
     @Column(name = "first-name", nullable = false, unique = true)
     private String firstname;
@@ -15,6 +30,21 @@ public class User extends BaseEntity {
     @Column(name = "last-name", nullable = false, unique = true)
     private String lastname;
 
-    @Column(name = "photo, nullable = true")
-    private Image photo;
+    @Embedded
+    @Column(name = "profile-photo", nullable = true)
+    private Image profilePhoto;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "points", nullable = false)
+    private int points = 0;
+
+    private List<String> habits;//TODO definir los hábitos de los usuarios. Definir entidad.
+
+    private List<String> level;//TODO definir los niveles de los usuarios. Definir Value Object.
+
+    private List<String> logros;//TODO definir los logros de los usuarios. Definir Value Object.
+
+    private boolean active;//? TODO lo incorporamos para el MVP active?
 }
