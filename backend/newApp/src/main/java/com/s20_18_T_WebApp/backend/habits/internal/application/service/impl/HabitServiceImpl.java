dac5ade04eb065289cc6e6b3c8d3b1624cc5422b
KevinLautaroSoto <kevinlautarosoto@gmail.com>
2025-01-17 19:50:31 -0300

    create HabitService interface and the implementation

---
+++ backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/habits/internal/application/service/impl/HabitServiceImpl.java
@@ -0,0 +1,6 @@
+package com.s20_18_T_WebApp.backend.habits.internal.application.service.impl;
+
+import com.s20_18_T_WebApp.backend.habits.internal.application.service.HabitService;
+
+public class HabitServiceImpl implements HabitService {
+}
---
+++ backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/habits/internal/package-info.java
@@ -0,0 +1,1 @@
+package com.s20_18_T_WebApp.backend.habits.internal;
