71d8ad843c95569559a140f5912e5cda37e34624
KevinLautaroSoto <kevinlautarosoto@gmail.com>
2025-01-17 19:41:30 -0300

    add custom queries to habitRepository

--- backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/habits/internal/infra/persistence/HabitRepository.java
+++ backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/habits/internal/infra/persistence/HabitRepository.java
@@ -1,10 +1,21 @@
 package com.s20_18_T_WebApp.backend.habits.internal.infra.persistence;
 
 import com.s20_18_T_WebApp.backend.habits.internal.domain.entity.Habit;
+import com.s20_18_T_WebApp.backend.habits.internal.domain.enums.HabitType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HabitRepository extends JpaRepository<Habit, Long> {
 
+    //Busca habito por nombre.
+    List<Habit> findByName(String name);
+
+    //Verifica si un habito con el mismo nombre ya existe.
+    boolean existsByName(String name);
+
+    //Obtiene habitos por tipo.
+    List<Habit> findByType(HabitType type);
 }
