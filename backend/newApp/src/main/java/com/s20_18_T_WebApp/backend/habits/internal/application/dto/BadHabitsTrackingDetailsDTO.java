85b70e2ca1852b86c21506db9162b1a1f17a6872
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2025-01-20 14:26:29 -0300

    Create BadHabitsTrackingDetailsDTO implementing HabitDetailsDTO interface

---
+++ backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/habits/internal/application/dto/BadHabitsTrackingDetailsDTO.java
@@ -0,0 +1,13 @@
+package com.s20_18_T_WebApp.backend.habits.internal.application.dto;
+
+import com.s20_18_T_WebApp.backend.habits.internal.domain.entity.habitTypes.BadHabitsTracking;
+
+public record BadHabitsTrackingDetailsDTO(
+    Integer daysAvoided
+) implements HabitDetailsDto {
+    public static BadHabitsTrackingDetailsDTO fromEntity(BadHabitsTracking habit) {
+        return new BadHabitsTrackingDetailsDTO(
+            habit.getDaysAvoided()
+        );
+    }
+}
--- backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/habits/internal/application/dto/HabitDetailsDto.java
+++ backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/habits/internal/application/dto/HabitDetailsDto.java
@@ -1,5 +1,5 @@
 package com.s20_18_T_WebApp.backend.habits.internal.application.dto;
 
 public sealed interface HabitDetailsDto
-        permits HealthyLivingDetailsDTO, PhysicalActivityDetailsDTO {
+        permits BadHabitsTrackingDetailsDTO, HealthyLivingDetailsDTO, PhysicalActivityDetailsDTO {
 }
