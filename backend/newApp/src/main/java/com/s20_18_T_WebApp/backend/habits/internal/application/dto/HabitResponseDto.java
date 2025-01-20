b655a5d522b7b17d89b4545e91f44408684c37fe
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2025-01-20 15:11:29 -0300

    implement HabitDetailsDTO interface in HabitResponseDto

--- backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/habits/internal/application/dto/HabitResponseDto.java
+++ backend/newApp/src/main/java/com/s20_18_T_WebApp/backend/habits/internal/application/dto/HabitResponseDto.java
@@ -1,10 +1,12 @@
 package com.s20_18_T_WebApp.backend.habits.internal.application.dto;
 
 import com.s20_18_T_WebApp.backend.habits.internal.application.dto.habitTypeDetails.HabitDetailsDto;
+import com.s20_18_T_WebApp.backend.habits.internal.domain.entity.Habit;
 import com.s20_18_T_WebApp.backend.habits.internal.domain.enums.HabitType;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Set;
 
 public record HabitResponseDto(
     Long id,
