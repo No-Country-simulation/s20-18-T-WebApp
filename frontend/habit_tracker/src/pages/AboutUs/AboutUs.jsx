afc9fffd603da3e74c519863ae122dd4f8bd75ad
cesar <cesarhernanruscica@gmail.com>
2025-02-05 16:31:36 -0300

    terminado

--- frontend/habit_tracker/src/pages/AboutUs/AboutUs.jsx
+++ frontend/habit_tracker/src/pages/AboutUs/AboutUs.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 
 import {  
   Avatar,
   Typography,
-  Box,
-  Button,  
+  Box   
 } from '@mui/material';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { Link } from 'react-router-dom';
-
-
 
 const cardStyles = {
   borderRadius: '1rem',
@@ -25,8 +21,7 @@ const cardStyles = {
     boxShadow: 3, // Un ligero efecto al pasar el ratón        
   },
   backgroundColor: "#FCFCFC",
-  border: "1px solid #DDD",
-  marginTop: "2em"
+  border: "1px solid #DDD",  
 }
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
@@ -36,73 +31,97 @@ const integrantes = [
     nombre: "Sara Alcantara",
     rol: "Product manager",
     imagen: "SaraAlcantara.png",
-    enlace: "",
+    enlaces: ['https://www.linkedin.com/in/sara-e-alcantara-r']
   },
   {
     nombre: "Santiago Garcia",
     rol: "UX/UI",
     imagen: "SantiagoGarcia.png",
-    enlace: "",
+    enlaces: ['https://www.linkedin.com/in/santiagogarciaa',
+              'https://www.behance.net/santiagogarciaa'
+    ]
   },
   {
     nombre: "Anadela Hernandez",
     rol: "UX/UI",
     imagen: "AnadelaHernandez.png",
-    enlace: "",
+    enlaces: ['https://www.linkedin.com/in/anadelahernandez',
+              'https://www.behance.net/anadelahernndez2'
+    ]
   },
   {
     nombre: "Cesar Ruscica",
     rol: "Frontend",
     imagen: "CesarRuscica.png",
-    enlace: "https://impulsainternet.com/ruscica",
+    enlaces: ["https://impulsainternet.com/ruscica"],
   },
   {
     nombre: "Jeniree Suarez",
     rol: "QA Tester",
     imagen: "JenireeSuarez.png",
-    enlace: "",
+    enlaces: []
   },
 ]
 
+const styles = {
+  cardContainer: {
+    display: "flex", 
+    flexDirection: "row",
+    flexWrap: 'wrap',
+    gap: "1.5em"}
+}
+
 export const AboutUs = () => {
+  
   return (
-    <div>
-      <h2>Sobre nosotos</h2>
-      <p>Quienes somos los que hicimos esta aplicacion. </p>
-      <div>      
+    <div >
+      <h2>Sobre nosotros</h2>
+      <p>Quienes somos los que hicimos esta aplicación. Por orden alfabético.</p>
+      <div style={styles.cardContainer}>      
       {integrantes.map((integrante, index) => (
         <Card sx={cardStyles} key={index}>
         <CardContent>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ display: 'flex',  alignItems: 'center', mb: "8px" }}>
             <Avatar sx={{ mr: 4, width:"70px", height: "70px" }}>          
               <img src={`${baseUrl}/images/integrantes/${integrante.imagen}`} alt={`imagen de ${integrante.nombre}`}
                 width="100%" height="auto"
               />
             </Avatar>
             <Box sx={{ width: '100%', display: "flex", flexDirection: "column", rowGap: "6px" }}>
-              <Typography fontWeight="bold">
+              <Typography fontWeight="bold" fontSize='20px'>
                 {integrante.nombre}
               </Typography>
               <Typography variant="body2" color="text.secondary">
                 {integrante.rol}
               </Typography>   
-
-              {
-              (integrante.enlace !== '') &&
-              <Button
-              component={Link}  // Usa el componente Link de react-router-dom
-              to={integrante.enlace} // Pasa la URL al atributo to
-              target="_blank"    // Abre el enlace en una nueva pestaña
-              rel="noopener noreferrer" // Agrega por seguridad cuando usas target="_blank"
-              variant="outlined"
-              endIcon={<OpenInNewIcon />}
-              >
-              {integrante.enlace}
-            </Button>
-            }
-
             </Box>
           </Box>  
+          <Box sx={{ width: '100%', display: "flex", flexDirection: "column", rowGap: "18px" }}>
+          {
+          (integrante.enlaces.length > 0) ?
+          integrante.enlaces.map((enlace, index)=> (
+        <Link             
+            key={index}              
+            to={enlace} 
+            target="_blank"    
+            rel="noopener noreferrer"                        
+            >
+            {/* <OpenInNewIcon /> */}
+            <Typography variant='body1' 
+              sx={{
+                wordBreak: 'break-all',  
+                backgroundColor: "#dce6fd",
+                color: '#2232ab',
+                padding: "10px",
+                borderRadius: "10px",
+                textAlign: "center"                
+                }}>
+              {enlace}
+            </Typography>
+        </Link>))
+        : null  // Use null instead of empty string
+        }
+        </Box>
         </CardContent>
       </Card>
       ))}
